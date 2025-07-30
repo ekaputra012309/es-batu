74d43992b63372c8be9769a9feab05324ecee16a
ekaputra012309 <eko012309@gmail.com>
2025-07-30 15:13:59 +0700

    remake print transaksi

--- resources/views/backend/transaksi/print_transaksi.blade.php
+++ resources/views/backend/transaksi/print_transaksi.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="shortcut icon" href="{{ asset('backend/img/logo.ico') }}" type="image/x-icon">
     <title>Transaksi Print</title>
@@ -12,36 +13,47 @@
             padding: 0;
             font-size: 11px;
         }
+
         h1 {
             text-align: center;
             font-size: 18px;
         }
+
         table {
             width: 100%;
             border-collapse: collapse;
-            margin: 0; /* Remove margin */
+            margin: 0;
+            /* Remove margin */
         }
-        th, td {
+
+        th,
+        td {
             text-align: left;
-            padding: 2px; /* Minimal padding */
-            line-height: 1.2; /* Adjust line height if needed */
+            padding: 2px;
+            /* Minimal padding */
+            line-height: 1.2;
+            /* Adjust line height if needed */
         }
+
         .footer {
             /* margin-top: 10px; */
             text-align: center;
         }
-        .detail-row {
-            border-bottom: 1px dashed #000;
-        }
+
         .price {
-            text-align: right; /* Align price to the right */
+            text-align: right;
+            /* Align price to the right */
         }
+
         .total-row {
             font-weight: bold;
-            text-align: right; /* Align total to the right */
+            text-align: right;
+            border-top: 0.1px solid #000;
+            /* Align total to the right */
         }
     </style>
 </head>
+
 <body>
     <h3 class="text-center">{{ config('app.name') }}</h3>
     <table>
@@ -66,16 +78,50 @@
     <h3>Detail Transaksi</h3>
     <table>
         @foreach ($transaksi->details as $detail)
-            <tr class="detail-row">
-                <td>{{ $detail->berat }} {{ $detail->satuan }} ( {{ $detail->qty }} pcs x Rp {{ number_format($detail->harga, 0, ',', '.') }} )</td>
+            <tr>
+                <td>{{ $detail->berat }} {{ $detail->satuan }} ( {{ $detail->qty }} pcs x Rp
+                    {{ number_format($detail->harga, 0, ',', '.') }} )</td>
                 <td class="price">Rp {{ number_format($detail->qty * $detail->harga, 0, ',', '.') }}</td>
-                <!-- <td class="price">Rp {{ number_format($detail->harga, 0, ',', '.') }}</td> -->
             </tr>
         @endforeach
         <tr class="total-row">
             <td>Total</td>
             <td class="price">Rp {{ number_format($transaksi->total, 0, ',', '.') }}</td>
         </tr>
+
+        @php
+            $totalBayar = $transaksi->bayar->sum('nominal');
+            $sisa = $transaksi->total - $totalBayar;
+        @endphp
+
+        <tr>
+            <td colspan="2"><strong>Detail Pembayaran:</strong></td>
+        </tr>
+        @foreach ($transaksi->bayar as $bayar)
+            <tr>
+                <td>- {{ \Carbon\Carbon::parse($bayar->created_at)->translatedFormat('d M Y H:i') }}</td>
+                <td class="price">Rp {{ number_format($bayar->nominal, 0, ',', '.') }}</td>
+            </tr>
+        @endforeach
+
+        <tr class="total-row">
+            <td>Total Dibayar</td>
+            <td class="price">Rp {{ number_format($totalBayar, 0, ',', '.') }}</td>
+        </tr>
+
+        @if ($transaksi->status == 0)
+            <tr>
+                <td colspan="2" class="text-center"><strong>Transaksi sudah lunas</strong></td>
+            </tr>
+        @else
+            <tr class="total-row">
+                <td>Sisa Pembayaran</td>
+                <td class="price text-danger">Rp {{ number_format($sisa, 0, ',', '.') }}</td>
+            </tr>
+            <tr>
+                <td colspan="2" class="text-center"> <strong>Harap segera melunasi sisa pembayaran</strong></td>
+            </tr>
+        @endif
     </table>
 
     <div class="footer">
@@ -83,4 +129,5 @@
         <p>Silakan Kunjungi Kami Lagi!</p>
     </div>
 </body>
+
 </html>
