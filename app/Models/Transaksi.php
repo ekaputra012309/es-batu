d00d1f13925e917167d5c1dcc374382448b46e70
ekaputra012309 <eko012309@gmail.com>
2025-08-03 19:18:49 +0700

    fix detay order by tanggal asc

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -22,7 +22,8 @@ class Transaksi extends Model
 
     public function details()
     {
-        return $this->hasMany(TransaksiDetail::class, 'table_transaksi_id');
+        // return $this->hasMany(TransaksiDetail::class, 'table_transaksi_id');
+        return $this->hasMany(TransaksiDetail::class, 'table_transaksi_id')->orderBy('tanggal', 'asc');
     }
 
     public function bayar()
